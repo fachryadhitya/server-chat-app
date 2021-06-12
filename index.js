60636b4a71ebc24823d0c8a78533cfb4ee0f52c7
fachry <fachryadhitya@gmail.com>
2021-06-12 17:18:24 +0700

    change message

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ io.on("connect", (socket) => {
     if (user) {
       io.to(user.room).emit("message", {
         user: "Admin",
-        text: `${user.name} has left.`,
+        text: `${user.name} has left the room.`,
       });
       io.to(user.room).emit("roomData", {
         room: user.room,
